889eddfd6f53e010b3f846723b9931f6f3318ea9
Jordy <jordy_van_opstal@live.nl>
2019-06-03 23:24:48 +0200

    Add ExpectedValue.

--- Continuous/ContinuousDistributionExtensions.cs
+++ Continuous/ContinuousDistributionExtensions.cs
@@ -1,4 +1,8 @@
-﻿namespace Chinchillada.Distributions
+﻿using System.Linq;
+using Chinchillada.Utilities;
+using Rewired;
+
+namespace Chinchillada.Distributions
 {
     using System;
 
@@ -20,6 +24,19 @@
 
                 float Bayes(float d) => prior.Weight(d) * likelihood(d).Weight(value);
             };
+        }
+
+        public static float ExpectedValue(
+            this IWeightedDistribution<float> distribution,
+            Func<float, float> function,
+            float start = 0f,
+            float end = 1f,
+            int buckets = 1000)
+        {
+            Func<float, float> value = x => function(x) * distribution.Weight(x);
+            Func<float, float> weight = distribution.Weight;
+
+            return value.Area(start, end, buckets) / weight.Area(start, end, buckets);
         }
 
         private static IDistribution<float> NormalAround(float value) => Normal.Distribution(value, 1f);
