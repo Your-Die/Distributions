4a1d4a51e064d8216f00370e748773492dea5335
Jordy <jordy_van_opstal@live.nl>
2020-12-12 14:08:53 +0100

    Improve serializable distributions
    Each of them now also implements their respective inferface.

--- Serializables/UniformDistribution.cs
+++ Serializables/UniformDistribution.cs
@@ -6,11 +6,16 @@ using UnityEngine;
 namespace Chinchillada.Distributions.Serializables
 {
     [Serializable]
-    public class UniformDistribution<T> : SerializableDistribution<T>
+    public class UniformDistribution<T> : SerializableDistribution<T, IDiscreteDistribution<T>>, IDiscreteDistribution<T>
     {
         [SerializeField, OnValueChanged(nameof(UpdateDistribution))]
         private ICollection<T> items= new List<T>();
         
-        protected override IDistribution<T> BuildDistribution() => DiscreteUniform<T>.Distribution(this.items);
+        protected override IDiscreteDistribution<T> BuildDistribution() => DiscreteUniform<T>.Distribution(this.items);
+        public IEnumerable<T> Support() => this.Distribution.Support();
+
+        public int Weight(T variable) => this.Distribution.Weight(variable);
+
+        float IWeightedDistribution<T>.Weight(T item) => this.Weight(item);
     }
 }
--- Serializables/WeightedDistribution.cs
+++ Serializables/WeightedDistribution.cs
@@ -6,11 +6,16 @@ using UnityEngine;
 namespace Chinchillada.Distributions.Serializables
 {
     [Serializable]
-    public class WeightedDistribution<T> : SerializableDistribution<T>
+    public class WeightedDistribution<T> : SerializableDistribution<T, IDiscreteDistribution<T>>, IDiscreteDistribution<T>
     {
         [SerializeField, OnValueChanged(nameof(UpdateDistribution))]
         private Dictionary<T, int> weightedItems = new Dictionary<T,int>();
      
-        protected override IDistribution<T> BuildDistribution() => this.weightedItems.ToWeighted();
+        protected override IDiscreteDistribution<T> BuildDistribution() => this.weightedItems.ToWeighted();
+        public IEnumerable<T> Support() => this.Distribution.Support();
+
+        int IDiscreteDistribution<T>.Weight(T variable) => this.Distribution.Weight(variable);
+
+        public float Weight(T item) => this.Distribution.Weight(item);
     }
 }
